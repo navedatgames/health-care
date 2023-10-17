13bdcd32aeb195ca843a2d7380e52852adb99c06
navedatgames <navedatgames@gmail.com>
2023-10-17 07:28:26 +0530

    project setup and config tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components";
+import styles from "./index.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-400 ml-47 mt-14 mr-0 mb-0 ">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../index.css";
+
+const Header = () => {
+  return <div className="font-mulish font-normal text-2xl">This is Header</div>;
+};
+
+export default Header;
